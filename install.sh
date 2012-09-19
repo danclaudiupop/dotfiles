bfc9a5aeb54d9e25491c6eb25ee8f5744e1b5212
Dan Claudiu Pop <danclaudiupop@gmail.com>
2012-09-19 12:24:34 +0300

    Rest of the files

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# wget -qO- https://github.com/danclaudiupop/dotfiles/raw/master/install.sh | /bin/bash
+
+if [ -d ~/.dotfiles ]; then
+    cd ~/.dotfiles
+    git pull
+else
+    git clone https://github.com/danclaudiupop/dotfiles.git ~/.dotfiles/
+    for cf in ~/.dotfiles/configs/*; do
+        ln -s "$cf" ~/.$(basename "$cf")
+    done
+fi
+
+cd ~/.dotfiles
+git submodule init
+git submodule update
