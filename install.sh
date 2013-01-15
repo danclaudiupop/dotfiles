2f567950db094055e4340d699f0d17cffdb3e8c4
danu <dan@ubuntul.(none)>
2013-01-15 16:42:07 +0200

    new install procedure

--- install.sh
+++ install.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
-# wget -qO- https://github.com/danclaudiupop/dotfiles/raw/master/install.sh | /bin/bash
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/$1"
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
 
 if [ -d ~/.dotfiles ]; then
+    cd ~/.dotfiles/.vim/bundle/vundle/
+    git pull origin master
     cd ~/.dotfiles
-    git pull
+    git pull origin master
+
+    sudo pip install pip --upgrade
+    sudo pip install flake8 --upgrade
+    vim -u ~/.dotfiles/.vimrc - +BundleInstall! +BundleClean! +qall
 else
-    git clone https://github.com/danclaudiupop/dotfiles.git ~/.dotfiles/
-    git clone https://github.com/gmarik/vundle.git ~/.dotfiles/configs/vim/bundle/vundle
-    for cf in ~/.dotfiles/configs/*; do
-        ln -s "$cf" ~/.$(basename "$cf")
+    git clone git://github.com/limpangel/dotfiles.git ~/.dotfiles
+    git clone https://github.com/gmarik/vundle.git ~/.dotfiles/.vim/bundle/vundle
+    cd ~/.dotfiles
+
+    for f in .vim .vimrc .tmux.conf .gitconfig .battery
+    do
+        link_file $f
     done
 
-    vim -u ~/.dotfiles/configs/vimrc - +BundleInstall +qall
+    sudo apt-get -y install python-pip
+    sudo pip install pip --upgrade
+    sudo pip install flake8 --upgrade
+    vim -u ~/.dotfiles/.vimrc - +BundleInstall +qall
 fi
