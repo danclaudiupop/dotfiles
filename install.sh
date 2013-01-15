64c3ac6b9c1150b135df8a1e023cb53aa9b4507f
Dan Claudiu Pop <danclaudiupop@gmail.com>
2013-01-15 14:59:04 +0200

    setup vundle

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ if [ -d ~/.dotfiles ]; then
     git pull
 else
     git clone https://github.com/danclaudiupop/dotfiles.git ~/.dotfiles/
+    git clone https://github.com/gmarik/vundle.git ~/.dotfiles/.vim/bundle/vundle
     for cf in ~/.dotfiles/configs/*; do
         ln -s "$cf" ~/.$(basename "$cf")
     done
+
+    vim +BundleInstall +qall
 fi
